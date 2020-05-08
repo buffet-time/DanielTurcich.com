92d0cc8533ac6b98ff80fe1570fc5e97ff5a63a6
buffet-time <me@danielTurcich.com>
2020-05-08 15:33:11 -0400

    Added JS to handle generating accronyms, will create UI/UX to work with it soon

--- src/components/acronym.component.ts
+++ src/components/acronym.component.ts
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Component from 'vue-class-component'
+import Words from '../assets/words.json'
 import Template from './acronym.component.html'
 import { Navbar } from './navbar.component'
 
@@ -10,5 +11,31 @@ import { Navbar } from './navbar.component'
 	}
 })
 export class Acronym extends Vue {
-	//
+	private wordsArray: string[] = Words
+
+	public mounted() {
+		console.log(this.getWordsFromProvidedAcronym('test'))
+	}
+
+	public getWordsStartingWith(letter: string): string[] {
+		return this.wordsArray.filter(word => {
+			if (word.startsWith(letter)) {
+				return word
+			}
+		})
+	}
+
+	public getWordsFromProvidedAcronym(acronym: string): string[] {
+		const wordsFromAcronym: string[] = []
+		const acronymArray: string[] = acronym.split('')
+
+		acronymArray.forEach(letter => {
+			const wordsArray = this.getWordsStartingWith(letter)
+			wordsFromAcronym.push(
+				wordsArray[Math.floor(Math.random() * wordsArray.length)]
+			)
+		})
+
+		return wordsFromAcronym
+	}
 }
