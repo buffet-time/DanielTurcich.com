a31d2b51add7da1f537c1309f855c68050d52e3e
Daniel Turcich <github@danielTurcich.com>
2021-09-25 14:01:08 -0400

    More work to trying to get vite builds working

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,5 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/ban-types */
-declare module '*.vue' {
-	import type { DefineComponent } from 'vue'
-	const component: DefineComponent<{}, {}, any>
-	export default component
-}
-
 declare module '*.json' {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	const value: any
 	export default value
 }
