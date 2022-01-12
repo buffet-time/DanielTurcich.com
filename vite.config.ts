8541261a323a9e0d2e768505298f965ce72ced7e
Daniel Turcich <github@danielTurcich.com>
2022-01-12 00:12:52 -0500

    Setting up Github Actions to autodeploy to Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
 	root: './src',
+	base: '/DanielTurcich.com/',
 
 	build: {
 		chunkSizeWarningLimit: 2068, // the size of acronymWords.json
